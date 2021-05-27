ac6ff61c810b1d946e76012808e8d503d8e3c0fb
Olga Dorosh <odorosh28@gmail.com>
2021-05-27 21:42:20 +0300

    fix hw

--- src/index.js
+++ src/index.js
@@ -2,25 +2,35 @@ import NewsApiService from './js/apiService';
 import getRefs from './js/getRefs';
 import photoCardTpl from './templates/photo-card.hbs';
 import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
 const refs = getRefs();
-
-refs.loadMoreBtn.disabled = true;
 
 const newsApiService = new NewsApiService();
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.galleryList.addEventListener('click', onOpenBigPhoto);
+
+function onOpenBigPhoto(e) {
+  if (e.target.classList.contains('image')) {
+    const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.img}" width="800" height="600">
+`);
+
+    instance.show();
+  }
+}
 
 function onSearch(e) {
   e.preventDefault();
   const form = e.currentTarget;
   newsApiService.query = form.elements.query.value;
-  refs.loadMoreBtn.disabled = false;
 
   if (!newsApiService.query.trim()) {
     alert('Enter your query!');
     clearPhotosList();
+    refs.loadMoreBtn.classList.add('is-hidden');
     return;
   }
 
@@ -30,19 +40,24 @@ function onSearch(e) {
     clearPhotosList();
     appendPhotosMarkUp(hits);
   });
+
+  refs.loadMoreBtn.classList.remove('is-hidden');
 }
 
 function appendPhotosMarkUp(hits) {
   refs.galleryList.insertAdjacentHTML('beforeend', photoCardTpl(hits));
-
-  refs.galleryList.scrollIntoView({
-    behavior: 'smooth',
-    block: 'end',
-  });
 }
 
-function onLoadMore() {
-  newsApiService.fetchImagesByKeyWord().then(appendPhotosMarkUp);
+async function onLoadMore() {
+  await newsApiService.fetchImagesByKeyWord().then(appendPhotosMarkUp);
+  setTimeout(scroll, 100);
+}
+
+function scroll() {
+  window.scrollTo({
+    top: document.documentElement.scrollHeight,
+    behavior: 'smooth',
+  });
 }
 
 function clearPhotosList() {
